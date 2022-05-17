62c45a17d55809b6cbb7faf7b2c71bfb1ebc749d
Loris Ercole <loris.ercole@gmail.com>
2022-05-17 16:02:58 +0200

    Update main + fix bug with NaN
    An error was raised when an optional float battery value (e.g.
    capacity.actual) was set to NaN. Now it is removed from the output
    dictionary that is used to define the BatterySample.

--- aurora/interface/main.py
+++ aurora/interface/main.py
@@ -3,27 +3,26 @@
 import pandas as pd
 import ipywidgets as ipw
 from IPython.display import display
-# from ..query import (update_available_samples, update_available_specs, update_available_recipies,
-#     query_available_samples, query_available_specs, query_available_recipies,
-#     write_pd_query_from_dict)
 from .sample import SampleFromId, SampleFromSpecs, SampleFromRecipe
 from .cycling import CyclingStandard, CyclingCustom
 from ..schemas.data_schemas import BatterySample, BatterySpecs
+from ..schemas.convert import _remove_empties_from_dict
 
 class MainPanel(ipw.VBox):
-    
+
     _SAMPLE_INPUT_LABELS = ['Select from ID', 'Select from Specs', 'Make from Recipe']
     _SAMPLE_INPUT_METHODS = ['id', 'specs', 'recipe']
     _TEST_METHOD_LABELS = ['Standardized', 'Customized']
     w_header = ipw.HTML(value="<h2>Aurora</h2>")
     _SAMPLE_BOX_LAYOUT = {'width': '90%', 'border': 'solid blue 2px', 'align_content': 'center', 'margin': '5px', 'padding': '5px'}
 
+    # SAMPLE SELECTION
     def return_selected_sample(self, sample_widget_obj):
-        self.selected_battery_sample = BatterySample.parse_obj(sample_widget_obj.selected_sample_dict)
+        self.selected_battery_sample = BatterySample.parse_obj(_remove_empties_from_dict(sample_widget_obj.selected_sample_dict))
         self.post_sample_selection()
 
     def return_selected_specs_recipe(self, sample_widget_obj):
-        self.selected_battery_specs = BatterySpecs.parse_obj(sample_widget_obj.selected_specs_dict)
+        self.selected_battery_specs = BatterySpecs.parse_obj(_remove_empties_from_dict(sample_widget_obj.selected_specs_dict))
         self.selected_recipe = sample_widget_obj.selected_recipe_dict
 
     def switch_to_recipe(self, specs_widget_obj):
@@ -43,6 +42,7 @@ class MainPanel(ipw.VBox):
         self.selected_battery_sample = None
         self.selected_battery_specs = None
         self.selected_recipe = None
+        self.selected_cycling_protocol = None
 
     def display_tested_sample_preview(self):
         self.w_test_sample.clear_output()
@@ -56,6 +56,19 @@ class MainPanel(ipw.VBox):
         if self.w_sample_selection_tab.selected_index is not None:
             return self._SAMPLE_INPUT_METHODS[self.w_sample_selection_tab.selected_index]
 
+    # CYCLING
+    def return_selected_protocol(self, cycling_widget_obj):
+        self.selected_cycling_protocol = cycling_widget_obj.protocol_steps_list
+        self.post_protocol_selection()
+
+    def post_protocol_selection(self):
+        "Close accordion, submit job to AiiDA"
+        if self.selected_battery_sample is None:
+            raise ValueError("A Battery sample was not selected!")
+        self.w_main_accordion.selected_index = None
+        print(f"Battery Sample:\n  {self.selected_battery_sample}\nCycling Protocol:\n  {self.selected_cycling_protocol}\nSubmitting to AiiDA...")
+
+    # MAIN
     def __init__(self):
         
         # initialize variables
@@ -76,7 +89,7 @@ class MainPanel(ipw.VBox):
         self.w_test_sample_label = ipw.HTML("Selected sample:")
         self.w_test_sample = ipw.Output(layout=self._SAMPLE_BOX_LAYOUT)
         self.w_test_standard = CyclingStandard(lambda x: x)
-        self.w_test_custom = CyclingCustom(lambda x: x)
+        self.w_test_custom = CyclingCustom(validate_callback_f=self.return_selected_protocol)
         self.w_test_method_tab = ipw.Tab(
             children=[self.w_test_standard, self.w_test_custom],
             selected_index=0)
@@ -86,7 +99,7 @@ class MainPanel(ipw.VBox):
             self.w_test_sample_label,
             self.w_test_sample,
             self.w_test_method_tab])
-        
+
         # MAIN ACCORDION
         self.w_main_accordion = ipw.Accordion(children=[self.w_sample_selection_tab, self.w_test])
         self.w_main_accordion.set_title(0, 'Sample selection')
