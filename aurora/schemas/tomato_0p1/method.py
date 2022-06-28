1f78ae140c84fb1b35e85b65fbac75b039d9500f
Loris Ercole <loris.ercole@gmail.com>
2022-06-28 15:13:18 +0200

    Implement tomato 0.1 compatible payload schemas
    These schemas can be used to communicate with tomato 0.1(.rc10+).
    Functions to convert aurora schemas to tomato's are included.
    The `schemas.tomato_0p1.TomatoPayload` schema can be exported in
    json/yaml format and passed to `ketchup`.

---
+++ aurora/schemas/tomato_0p1/method.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from pydantic import BaseModel, Extra
+from typing import Literal, Sequence
+from aurora.schemas.cycling import ElectroChemSequence
+
+
+class Method(BaseModel, extra=Extra.allow):
+    device: Literal["MPG2", "worker"]
+    technique: str
+    # parameter_1
+    # parameter_2
+
+
+def convert_electrochemsequence_to_method_list(elchemsequence: ElectroChemSequence) -> Method:
+    """
+    Convert an ElectroChemSequence into a list of Method.
+    Parameter values that are None will be ignored.
+    """
+
+    sequence = []
+    for step in elchemsequence.method:
+        parameters = {name: param.value for name, param in step.parameters.items() if param.value is not None}
+        
+        sequence.append(Method(**{
+            'device': step.device,
+            'technique': step.technique,
+            }, **parameters)
+        )
+
+    return sequence
