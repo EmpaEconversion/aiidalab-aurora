0e76e6042fe57ae36bfc9d068e804015697acb7c
Loris Ercole <loris.ercole@gmail.com>
2022-08-10 16:41:55 +0200

    Use `dgbowl-schemas`, define conversion functions
    Fixes #6
    The `dgbowl/dgbowl-schemas` are loaded directly into the
    `aurora.schemas.dgbowl_schemas.converters` module. Functions to convert
    aurora schemas into dgbowl-schemas are provided.
    For simplicity, this mapping can be performed by accessing the elements
    of the `converters` dictionary:
    e.g. to obtain a dgbowl-compatible `Sample` (payload version 0.2) from
    an aurora `BatterySample` object, do this:
    ```
    from aurora.schemas.battery import BatterySample
    from aurora.schemas.dgbowl_schemas import conversion_map
    sample = BatterySample(....)
    converted_sample = conversion_map["0.2"]["sample"](sample)
    ```

--- aurora/schemas/dgbowl_schemas/converters/method.py
+++ aurora/schemas/dgbowl_schemas/converters/method.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from pydantic import BaseModel, Extra
-from typing import Literal, Sequence
 from aurora.schemas.cycling import ElectroChemSequence
+from pydantic import BaseModel
 
 
-class Method(BaseModel, extra=Extra.allow):
-    device: Literal["MPG2", "worker"]
-    technique: str
-    # parameter_1
-    # parameter_2
-
-
-def convert_electrochemsequence_to_method_list(elchemsequence: ElectroChemSequence) -> Method:
+def electrochemsequence_to_method_list_0(
+    elchemsequence: ElectroChemSequence,
+    MethodSchema: BaseModel):
     """
     Convert an ElectroChemSequence into a list of Method.
     Parameter values that are None will be ignored.
+
+    Compatible with the following dgbowl-schemas payload versions:
+        [0.1, 0.2]
     """
+    _COMPATIBLE_PAYLOAD_VERSIONS = ["0.1", "0.2"]
 
     if not isinstance(elchemsequence, ElectroChemSequence):
         if isinstance(elchemsequence, dict):
@@ -27,7 +25,7 @@ def convert_electrochemsequence_to_method_list(elchemsequence: ElectroChemSequen
     for step in elchemsequence.method:
         parameters = {name: param.value for name, param in step.parameters.items() if param.value is not None}
         
-        sequence.append(Method(**{
+        sequence.append(MethodSchema(**{
             'device': step.device,
             'technique': step.technique,
             }, **parameters)
