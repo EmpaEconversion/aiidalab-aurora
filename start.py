815fb7e77d0627ad702d50ca58d8ce273d549afc
ramirezfranciscof <ramirezfranciscof@gmail.com>
2022-12-30 14:38:40 +0000

    Reorganize file structure (1)

--- start.py
+++ start.py
@@ -4,9 +4,9 @@ import ipywidgets as ipw
 
 template = """
 <h1> Aurora app </h1>
-<h2>&clubs; <a title="Submit experiment" href="{appbase}/Aurora-app.ipynb" target="_blank">Submit Experiment</a></h1>
-<h2>&clubs; <a title="Manage Samples" href="{appbase}/manage_samples.ipynb" target="_blank">Manage Samples</a></h2>
-<h2>&clubs; <a title="Visualize Results" href="{appbase}/Results-visualizer.ipynb" target="_blank">Results visualizer</a></h2>
+<h2>&clubs; <a title="Submit experiment" href="{appbase}/notebook_experiments.ipynb" target="_blank">Submit Experiment</a></h1>
+<h2>&clubs; <a title="Manage Samples" href="{appbase}/notebook_samples.ipynb" target="_blank">Manage Samples</a></h2>
+<h2>&clubs; <a title="Visualize Results" href="{appbase}/notebook_results.ipynb" target="_blank">Results visualizer</a></h2>
 """
 
 
