3c5467d5b57a960ac714d876fb16422e44e56520
Edan Bainglass <edan.bainglass@gmail.com>
2023-07-03 04:45:38 +0000

    Implement `StatisticalPlotPresenter` parent

--- aurora/results/plot/presenters/parents.py
+++ aurora/results/plot/presenters/parents.py
@@ -12,3 +12,15 @@ class MultiSeriesPlotPresenter(PlotPresenter):
             for eid, dataset in self.model.data.items():
                 if dataset:
                     self.plot_series(eid, dataset)
+
+
+class StatisticalPlotPresenter(PlotPresenter):
+    """
+    docstring
+    """
+
+    def draw(self) -> None:
+        """docstring"""
+        with self.view.plot:
+            if self.model.data:
+                self.plot_series(0, self.model.data)
