ca20fe651e2dcc2a722e24816e5b0cf60fb0953d
Edan Bainglass <edan.bainglass@gmail.com>
2023-07-14 14:41:02 +0000

    Implement down-selection controls for C vs. cycle

--- aurora/results/plot/presenters/capacity_cycle.py
+++ aurora/results/plot/presenters/capacity_cycle.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from contextlib import suppress
+from typing import List, Tuple
 
 import ipywidgets as ipw
 import numpy as np
@@ -28,6 +29,22 @@ class CapacityCyclePlotPresenter(MultiSeriesPlotPresenter):
 
         super().__init__(model, view)
 
+        _range = ipw.Text(
+            layout={
+                "width": "90%",
+            },
+            description="range",
+            placeholder="start:end:step",
+        )
+
+        points = ipw.Text(
+            layout={
+                "width": "90%",
+            },
+            description="points",
+            placeholder="e.g 0 5 -1",
+        )
+
         electrode = ipw.RadioButtons(
             layout={},
             options=[
@@ -40,6 +57,8 @@ class CapacityCyclePlotPresenter(MultiSeriesPlotPresenter):
         )
 
         controls = {
+            'range': _range,
+            'points': points,
             'electrode': electrode,
         }
 
@@ -55,6 +74,7 @@ class CapacityCyclePlotPresenter(MultiSeriesPlotPresenter):
         """docstring"""
         x, y = (np.array(a) for a in self.extract_data(dataset))
         y *= self.weights[eid].get(self.view.electrode.value, 1)
+        x, y = self._down_select(x, y)
         color = self.model.colors.get(eid)
         self.model.ax.plot(x, y, '.', label=eid, color=color)
 
@@ -67,11 +87,88 @@ class CapacityCyclePlotPresenter(MultiSeriesPlotPresenter):
 
         super()._set_event_handlers()
 
+        self.view.range.observe(
+            names='value',
+            handler=self._on_range_change,
+        )
+
+        self.view.points.observe(
+            names='value',
+            handler=self._on_points_change,
+        )
+
         self.view.electrode.observe(
             names='value',
             handler=self._on_electrode_change,
         )
 
+    def _on_range_change(self, _=None) -> None:
+        """docstring"""
+
+        self.view.points.unobserve(
+            names='value',
+            handler=self._on_points_change,
+        )
+
+        self.view.points.value = ''
+
+        self.view.points.observe(
+            names='value',
+            handler=self._on_points_change,
+        )
+
+        self.refresh()
+
+    def _on_points_change(self, _=None) -> None:
+        """docstring"""
+
+        self.view.range.unobserve(
+            names='value',
+            handler=self._on_range_change,
+        )
+
+        self.view.range.value = ''
+
+        self.view.range.observe(
+            names='value',
+            handler=self._on_range_change,
+        )
+
+        self.refresh()
+
     def _on_electrode_change(self, _=None) -> None:
         """docstring"""
         self.refresh(skip_x=True)
+
+    def _down_select(self, x: np.ndarray, y: np.ndarray) -> Tuple:
+        """docstring"""
+        x, y = self._filter_range(x, y)
+        x, y = self._filter_points(x, y)
+        return (x, y)
+
+    def _filter_range(self, x: np.ndarray, y: np.ndarray) -> Tuple:
+        """docstring"""
+
+        start, end, step = None, None, None
+        args: List[str] = self.view.range.value.split(':')
+
+        if args:
+            with suppress(Exception):
+                start = int(args[0])
+
+        if len(args) > 1:
+            with suppress(Exception):
+                end = int(args[1]) + 1 or None
+
+        if len(args) > 2:
+            with suppress(Exception):
+                step = int(args[2])
+
+        return (x[start:end:step], y[start:end:step])
+
+    def _filter_points(self, x: np.ndarray, y: np.ndarray) -> Tuple:
+        """docstring"""
+        raw_list: List[str] = self.view.points.value.strip().split()
+        points = [int(p) for p in raw_list if p.strip('-').isnumeric()]
+        valid = [p for p in set(points) if -len(x) - 1 <= p < len(x)]
+        return (x[valid], y[valid]) if valid else (x, y)
