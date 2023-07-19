3f889c8b600a1622c54734248608d922040a5aee
Edan Bainglass <edan.bainglass@gmail.com>
2023-07-19 12:09:02 +0000

    Discard old visualizer

--- aurora/results/analyze/__init__.py
+++
@@ -1,7 +0,0 @@
-from .output_explorer import OutputExplorerComponent
-from .results_plotter import ResultsPlotterComponent
-
-__all__ = [
-    "OutputExplorerComponent",
-    "ResultsPlotterComponent",
-]
