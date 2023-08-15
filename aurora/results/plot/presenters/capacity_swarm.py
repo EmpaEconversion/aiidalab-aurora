93ef2eceec9b054a2b76fbc4c13b5a119cbd9884
Edan Bainglass <edan.bainglass@gmail.com>
2023-08-15 13:12:19 +0000

    Implement down-selection controls for swarm plot

--- aurora/results/plot/presenters/capacity_swarm.py
+++ aurora/results/plot/presenters/capacity_swarm.py
@@ -1,4 +1,5 @@
-from typing import Dict
+import contextlib
+from typing import Dict, List
 
 import ipywidgets as ipw
 import numpy as np
@@ -23,6 +24,29 @@ class CapacitySwarmPlotPresenter(StatisticalPlotPresenter):
 
     max_cycle = -1
 
+    BOX_PROPS = {
+        'boxprops': {
+            'facecolor': (0.25, 0.25, 0.25, 0.1),
+        },
+        'linewidth': 0.25,
+    }
+
+    HIDDEN_BOX_PROPS = {
+        'boxprops': {
+            'facecolor': 'none',
+            'edgecolor': 'none'
+        },
+        'medianprops': {
+            'color': 'none'
+        },
+        'whiskerprops': {
+            'color': 'none'
+        },
+        'capprops': {
+            'color': 'none'
+        }
+    }
+
     def __init__(
         self,
         model: PlotModel,
@@ -37,9 +61,37 @@ class CapacitySwarmPlotPresenter(StatisticalPlotPresenter):
                 "width": "140px",
             },
             description="# to show",
-            min=0,
+            min=1,
             max=10,
             value=10,
+        )
+
+        draw_box = ipw.Checkbox(
+            layout={},
+            description='show box',
+            value=True,
+        )
+
+        draw_swarm = ipw.Checkbox(
+            layout={},
+            description='show swarm',
+            value=True,
+        )
+
+        _range = ipw.Text(
+            layout={
+                "width": "190px",
+            },
+            description="range",
+            placeholder="start:end:step",
+        )
+
+        points = ipw.Text(
+            layout={
+                "width": "90%",
+            },
+            description="points",
+            placeholder="e.g 0 5 -1",
         )
 
         electrode = ipw.RadioButtons(
@@ -55,6 +107,10 @@ class CapacitySwarmPlotPresenter(StatisticalPlotPresenter):
 
         controls = {
             'num_cycles': num_cycles,
+            'draw_box': draw_box,
+            'draw_swarm': draw_swarm,
+            'range': _range,
+            'points': points,
             'electrode': electrode,
         }
 
@@ -93,31 +149,45 @@ class CapacitySwarmPlotPresenter(StatisticalPlotPresenter):
 
     def plot_series(self, eid: int, dataset: dict) -> None:
         """docstring"""
-
         data = self.extract_data(dataset)
+        data = self._down_select(data)
+        self._plot_boxplot(data, draw=self.view.draw_box.value)
+        self._plot_swarmplot(data, draw=self.view.draw_swarm.value)
 
+    ###########
+    # PRIVATE #
+    ###########
+
+    def _plot_boxplot(self, data: pd.DataFrame, draw=True) -> None:
+        """docstring"""
         sns.boxplot(
             ax=self.model.ax,
             data=data,
             x=self.X_LABEL,
             y=self.Y_LABEL,
             orient="v",
-            boxprops={"facecolor": (.25, .25, .25, .1)},
-            linewidth=0.25,
+            **self.BOX_PROPS if draw else self.HIDDEN_BOX_PROPS,
         )
 
-        sns.swarmplot(
-            ax=self.model.ax,
-            data=data,
-            x=self.X_LABEL,
-            y=self.Y_LABEL,
-            orient="v",
-            hue='eid',
-        )
+    def _plot_swarmplot(self, data: pd.DataFrame, draw=True) -> None:
+        """docstring"""
+        if draw:
+            sns.swarmplot(
+                ax=self.model.ax,
+                data=self._get_swarm_copy(data),
+                x=self.X_LABEL,
+                y=self.Y_LABEL,
+                orient="v",
+                hue='eid',
+            )
 
-    ###################
-    # PRIVATE METHODS #
-    ###################
+    def _get_swarm_copy(self, data: pd.DataFrame) -> pd.DataFrame:
+        """docstring"""
+        swarm_data = data.copy(deep=True)
+        x_series = swarm_data[self.X_LABEL]
+        old_new_map = {old: new for new, old in enumerate(x_series.unique())}
+        swarm_data[self.X_LABEL] = x_series.apply(lambda old: old_new_map[old])
+        return swarm_data
 
     def _set_max_cycle(self, dataset: dict) -> None:
         """docstring"""
@@ -153,11 +223,104 @@ class CapacitySwarmPlotPresenter(StatisticalPlotPresenter):
             handler=self.refresh,
         )
 
+        self.view.draw_box.observe(
+            names='value',
+            handler=self.refresh,
+        )
+
+        self.view.draw_swarm.observe(
+            names='value',
+            handler=self.refresh,
+        )
+
+        self.view.range.observe(
+            names='value',
+            handler=self._on_range_change,
+        )
+
+        self.view.points.observe(
+            names='value',
+            handler=self._on_points_change,
+        )
+
         self.view.electrode.observe(
             names='value',
             handler=self._on_electrode_change,
         )
 
+    def _on_range_change(self, _=None) -> None:
+        """docstring"""
+
+        self.view.points.unobserve(
+            names='value',
+            handler=self._on_points_change,
+        )
+
+        self.view.points.value = ''
+
+        self.view.points.observe(
+            names='value',
+            handler=self._on_points_change,
+        )
+
+        self.refresh()
+
+    def _on_points_change(self, _=None) -> None:
+        """docstring"""
+
+        self.view.range.unobserve(
+            names='value',
+            handler=self._on_range_change,
+        )
+
+        self.view.range.value = ''
+
+        self.view.range.observe(
+            names='value',
+            handler=self._on_range_change,
+        )
+
+        self.refresh()
+
     def _on_electrode_change(self, _=None) -> None:
         """docstring"""
         self.refresh(skip_x=True)
+
+    def _down_select(self, data: pd.DataFrame) -> pd.DataFrame:
+        """docstring"""
+        data = self._filter_range(data)
+        data = self._filter_points(data)
+        return data
+
+    def _filter_range(self, data: pd.DataFrame) -> pd.DataFrame:
+        """docstring"""
+
+        start, end, step = None, None, None
+        args: List[str] = self.view.range.value.split(':')
+
+        max_cycle = data[self.X_LABEL].max()
+
+        if args:
+            with contextlib.suppress(Exception):
+                start = int(args[0])
+
+        if len(args) > 1:
+            with contextlib.suppress(Exception):
+                end = int(args[1]) + 1 or None
+
+        if len(args) > 2:
+            with contextlib.suppress(Exception):
+                step = int(args[2])
+
+        _range = list(np.array(range(max_cycle + 1))[start:end:step])
+
+        return data.query(f'{self.X_LABEL} == {_range}') if _range else data
+
+    def _filter_points(self, data: pd.DataFrame) -> pd.DataFrame:
+        """docstring"""
+        raw_list: List[str] = self.view.points.value.strip().split()
+        points = [int(p) for p in raw_list if p.strip('-').isnumeric()]
+        max_cycle = data[self.X_LABEL].max() + 1
+        valid = [p for p in set(points) if -max_cycle - 1 <= p < max_cycle]
+        _range = list(np.array(range(max_cycle))[valid])
+        return data.query(f'{self.X_LABEL} == {_range}') if _range else data
